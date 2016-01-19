db3bb58497d6b6845d2551d6915ec091bc7d963e
Dan Phiffer <dan@phiffer.org>
2016-01-19 15:59:01 -0800

    🔧 Added VERSION to setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,40 @@
 #!/usr/bin/env python
 
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.meta.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+desc = open("README.md").read()
+version = open("VERSION").read()
 
 setup(
     name='mapzen.whosonfirst.meta',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.meta'],
-    version='0.8',
+    version=version,
     description='Simple Python utilities for working with Who\'s On First meta files',
     author='Mapzen',
     url='https://github.com/mapzen/py-mapzen-whosonfirst-meta',
     install_requires=[
         'geojson',
         'atomicwrites',
-        'mapzen.whosonfirst.utils>=0.17',
+        'mapzen.whosonfirst.utils>=0.18',
         ],
     dependency_links=[
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master#egg=mapzen.whosonfirst.utils-0.17',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master#egg=mapzen.whosonfirst.utils-0.18',
         ],
     packages=packages,
     scripts=[
         'scripts/wof-dump-meta',
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-meta/releases/tag/v0.8',
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-meta/releases/tag/' + version,
     license='BSD')
